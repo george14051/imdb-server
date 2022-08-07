55e81456d7df5c0ec48cf1de991887bddf4f54a5
George <george14051@gmail.com>
2022-08-07 04:14:33 +0300

    incom

--- server/index.js
+++ server/index.js
@@ -25,7 +25,7 @@ app.get("/api", (req, res) => {
 app.use('/api', router);
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+    res.sendFile(path.join(__dirname + '../client/build/index.html'));
 });
 
 app.listen(PORT, () => {
