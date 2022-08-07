e0c87f18e39788112a0d62f8f53dc1a81ee9a418
George <george14051@gmail.com>
2022-08-07 05:13:31 +0300

    incom

--- index.js
+++ index.js
@@ -34,7 +34,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname + './client/build/index.html'));
+    res.sendFile(path.join('./client/build/index.html'));
 });
 
 app.listen(PORT, () => {
