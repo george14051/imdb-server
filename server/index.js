194125b4b15c43bd2ab4c50d9973cd2c20103d5f
George <george14051@gmail.com>
2022-08-07 03:50:03 +0300

    initial

---
+++ server/index.js
@@ -0,0 +1,33 @@
+import { router } from './routes/router.js'
+import cors from 'cors'
+import express from 'express';
+import bodyParser from 'body-parser';
+import path from 'path';
+
+
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, 'client/build')));
+
+app.use(cors());
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/api", (req, res) => {
+    res.json({ message: "Hello from server!" });
+});
+
+app.use('/api', router);
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname + '/client/build/index.html'));
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on ${PORT}`);
+});
